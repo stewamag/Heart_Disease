e5a0646f2df4a1f43c5936361991e799ca1c4eb7
Aaron McCarty <amccartya@gmail.com>
2022-11-07 16:25:10 -0800

    Updated Web Pages to run on Flask

--- app.py
+++ app.py
@@ -8,29 +8,40 @@ import os
 
 #Load App and Database
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:db_password@127.0.0.1:5432/Heart_Ache3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:db_password@127.0.0.1:5432/Heart_Ache'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 sql = SQLAlchemy(app)
 
-
+#<img src="../Images/heart_pill.jpg" alt="headerpic" width = "100%" height="500">
 #Set up app route
 @app.route("/")
 def index():
-    #heart = sql.db.Heart.find_one()
     return render_template("index.html")
 
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,4)
-    loaded_model = pk.load()
-@app.route("/")
+@app.route("/home")
+def home():
+    #they clicked the nav bar
+    return render_template("index.html")
+
+
+@app.route("/states")
 def states():
     return render_template("states.html")
 
-@app.route("/")
-def states():
-    return render_template("FastFoodData.html")
+@app.route("/checkyourheart")
+def checkheart():
+    #heart = sql.db.Heart.find_one()
+    return render_template("checkheart.html")
 
+@app.route("/results")
+def results():
+    #heart results from sql 
+    return render_template('results.html')
 
+#Define other code for use
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,4)
+    loaded_model = pk.load()
 
 if __name__ == "__main__":
    app.run()
